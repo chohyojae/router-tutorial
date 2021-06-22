57ccb82c3e3e7550db4ea598e9cd5a144bbefb9c
hjcho <hjcho@seegene.com>
2021-06-22 14:54:33 +0900

    url query

--- src/About.js
+++ src/About.js
@@ -1,9 +1,24 @@
 import React from 'react';
-const About = () => {
+import qs from 'qs';
+
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+
+  const showDetail =
+    'true'.localeCompare(query.detail, undefined, {
+      sensitivity: 'base',
+    }) === 0;
+  const showNumber = Boolean(query.number);
+
   return (
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트입니다.</p>
+      {showDetail && <p>detail 값을 true로 설정하셨군요!</p>}
+      <br />
+      {showNumber && <p>#{query.number}</p>}
     </div>
   );
 };
